5162ee70e07b777e447e7f74dd6029a37e84bece
diablofied <dylanpribadi@gmail.com>
2022-09-21 20:03:47 +0700

    assignment 2 first push

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,8 @@
-# TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import request_catalogs
+
+appname = 'katalog'
+
+urlpatterns = [
+    path('', request_catalogs, name='request_catalogs')
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# TODO: Create your views here.
+from katalog.models import CatalogItem
+
+def request_catalogs(request):
+    data = CatalogItem.objects.all()
+
+    # data yang mau dipass ke html
+    context = {
+        'name' : 'dylan pinter',
+        'student_id' : 2106720872,
+        'catalogs' : data
+    }
+
+    # bakal ngerender data ke html
+    # data mapping
+    return render(
+        request,
+        'katalog.html',
+        context,
+    )
